6a48583986880f8e2bb87f02abdc8943af6cfe00
yunyuuma <gikiin67@gmail.com>
2026-02-13 13:40:05 +0900

    入力ミス防止のための修正

--- JankenApp.java
+++ JankenApp.java
@@ -12,22 +12,28 @@ public class JankenApp {
         System.out.print("0:グー 1:チョキ 2:パー → ");
 
         int yuma = scanner.nextInt();
+
+        if (yuma < 0 || yuma > 2) {
+            System.out.println("0〜2を入力してください！");
+            scanner.close();
+            return;
+        }
+
         int PPC = random.nextInt(3);
 
-        System.out.println("あなた: " + hands[yuma]);
-        System.out.println("CPU: " + hands[PPC]);
+        System.out.println("ゆうま: " + hands[yuma]);
+        System.out.println("PC: " + hands[PPC]);
 
         if (yuma == PPC) {
             System.out.println("あいこ！");
         } else if ((yuma == 0 && PPC == 1) ||
                    (yuma == 1 && PPC == 2) ||
                    (yuma == 2 && PPC == 0)) {
-            System.out.println("あなたの勝ち！");
+            System.out.println("ゆうまの勝ち！");
         } else {
-            System.out.println("あなたの負け！");
+            System.out.println("ゆうまの負け！");
         }
 
         scanner.close();
     }
 }
-
